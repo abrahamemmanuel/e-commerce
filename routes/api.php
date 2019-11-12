3eb850e03a9f6186e4d120eea3f4c270a2d49161
Abraham Emmanuel <info@zetamindgroup.com>
2019-11-12 13:41:57 +0100

    Created Model -a & modified the readme.md file

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,16 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// API resource
+Route::apiResource('products', 'ProductController');
+
+// Products reviews
+Route::group(['prefix' => 'products'], function () {
+    Route::apiResource('/{product}/reviews', 'ReviewController');
 });
